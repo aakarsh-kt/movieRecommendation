909c1c10626d64d5ee22acb3c1c0ffda9fd0977a
Akarsh Katiyar <akarshofficial009@gmail.com>
2024-04-20 18:24:02 +0530

    Built the app

--- server/app.py
+++ server/app.py
@@ -66,6 +66,130 @@
 #     app.run(debug=True)
 
 
+# from flask import Flask, request, jsonify
+# import numpy as np
+# import pandas as pd
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.svm import SVC
+# from sklearn.neighbors import NearestNeighbors
+# from sklearn.cluster import KMeans
+# from scipy.sparse import csr_matrix
+# from flask_cors import CORS
+# app = Flask(__name__)
+# CORS(app)
+# # Load Data
+# def load_data(movies_path, ratings_path):
+#     movies = pd.read_csv(movies_path)
+#     ratings = pd.read_csv(ratings_path)
+#     return movies, ratings
+
+# # Preprocessing Data
+# def preprocess_data(ratings, min_user_votes = 15, min_movie_votes = 31):
+#     user_counts = ratings['userId'].value_counts()
+#     movie_counts = ratings['movieId'].value_counts()
+    
+#     ratings = ratings[ratings['userId'].isin(user_counts[user_counts >= min_user_votes].index)]
+#     ratings = ratings[ratings['movieId'].isin(movie_counts[movie_counts >= min_movie_votes].index)]
+    
+#     final_dataset = ratings.pivot(index='movieId', columns='userId', values='rating').fillna(0)
+#     return final_dataset
+
+# # Train Linear Kernel SVM Model
+# def train_svm_model(movies, tfidf_vectorizer):
+#     tfidf_matrix = tfidf_vectorizer.fit_transform(movies['genres'])
+#     svm_model = SVC(kernel='linear')
+#     svm_model.fit(tfidf_matrix, movies['title'])
+#     return svm_model, tfidf_matrix
+
+# # Movie Recommendation using Linear Kernel SVM
+# def get_movie_recommendation_svm(movie_name, model, vectorizer, movies_df, tfidf_matrix, k = 10):
+#     movie_idx = movies_df[movies_df['title'].str.contains(movie_name, case=False)].index
+#     if len(movie_idx) > 0:
+#         movie_features = vectorizer.transform(movies_df.iloc[movie_idx]['genres'])
+#         similarity_scores = cosine_similarity(movie_features, tfidf_matrix)
+#         similar_movies_idx = np.argsort(similarity_scores[0])[::-1][1:k+1] 
+#         recommendations = movies_df.iloc[similar_movies_idx][['title', 'genres']]
+#         return recommendations.to_dict('records')
+#     else:
+#         return [{"Title": "No movies found. Please check your input."}]
+
+# # Train KNN Model
+# def train_knn_model(user_movie_ratings):
+#     knn_model = NearestNeighbors(metric='cosine', algorithm='brute')
+#     knn_model.fit(user_movie_ratings)
+#     return knn_model
+
+# # Movie Recommendation using KNN
+# def get_movie_recommendation_knn(movie_id, model, user_movie_ratings, k = 10):
+#     distances, indices = model.kneighbors(user_movie_ratings.loc[movie_id].values.reshape(1, -1), n_neighbors = k+1)
+#     movie_recommendations = []
+#     for i in range(1, len(distances.flatten())):
+#         movie_recommendations.append({'Title': user_movie_ratings.index[indices.flatten()[i]], 'Distance': distances.flatten()[i]})
+#     return movie_recommendations
+
+# # Train K-Means Model
+# def train_kmeans_model(user_movie_ratings, num_clusters):
+#     kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+#     kmeans.fit(user_movie_ratings)
+#     return kmeans, user_movie_ratings
+
+# # Recommendation for the user using K-Means
+# def recommend_movies(target_user_id, user_movie_ratings, cluster_labels):
+#     target_user_cluster = user_movie_ratings.loc[target_user_id, 'cluster']
+#     similar_users = user_movie_ratings[user_movie_ratings['cluster'] == target_user_cluster].index
+#     similar_users = similar_users[similar_users != target_user_id]
+#     target_user_ratings = user_movie_ratings.loc[target_user_id].values.reshape(1, -1)
+#     similar_users_ratings = user_movie_ratings.loc[similar_users]
+#     similarities = cosine_similarity(target_user_ratings, similar_users_ratings)[0]
+#     top_similar_users = similar_users[np.argsort(similarities)[::-1]][:5]
+#     top_movies = user_movie_ratings.loc[top_similar_users].mean().sort_values(ascending=False)
+#     recommendations = top_movies.head(10)
+#     return recommendations.to_dict()
+
+# # Main Route
+# @app.route('/recommend', methods=['GET'])
+# def recommend():
+#     movie_name = request.args.get('movie')
+#     model = request.args.get('model')
+
+#     if movie_name is None or model is None:
+#         return jsonify({"error": "Please provide both movie and model parameters."}), 400
+
+#     if model == 'svm':
+#         recommendations = get_movie_recommendation_svm(movie_name, svm_model, tfidf_vectorizer, movies, tfidf_matrix)
+#     elif model == 'knn':
+#         movie_id = int(movie_name)
+#         recommendations = get_movie_recommendation_knn(movie_id, knn_model, final_dataset)
+#     elif model == 'kmeans':
+#         target_user_id = int(movie_name)
+#         recommendations = recommend_movies(target_user_id, clustered_ratings, kmeans_model.labels_)
+#     else:
+#         return jsonify({"error": "Invalid model parameter. Please choose from 'svm', 'knn', or 'kmeans'."}), 400
+
+#     return jsonify(recommendations)
+
+# if __name__ == '__main__':
+#     # Load Data
+#     movies_path = 'https://raw.githubusercontent.com/Rakshitx1/Movie-Recomendation-System/master/Dataset/movies.csv'
+#     ratings_path = 'https://raw.githubusercontent.com/Rakshitx1/Movie-Recomendation-System/master/Dataset/ratings.csv'
+#     movies, ratings = load_data(movies_path, ratings_path)
+
+#     # Preprocess Data
+#     final_dataset = preprocess_data(ratings)
+
+#     # Train Linear Kernel SVM Model
+#     tfidf_vectorizer = TfidfVectorizer()
+#     svm_model, tfidf_matrix = train_svm_model(movies, tfidf_vectorizer)
+
+#     # Train KNN Model
+#     knn_model = train_knn_model(final_dataset)
+
+#     # Train K-Means Model
+#     num_clusters = 9  # Based on the previous analysis
+#     kmeans_model, clustered_ratings = train_kmeans_model(final_dataset, num_clusters)
+
+#     app.run(debug=True)
 from flask import Flask, request, jsonify
 import numpy as np
 import pandas as pd
@@ -76,16 +200,30 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import KMeans
 from scipy.sparse import csr_matrix
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app)
+CORS(app )
+
+
+# Define global variables for models and data
+movies = None
+ratings = None
+final_dataset = None
+tfidf_vectorizer = None
+svm_model = None
+tfidf_matrix = None
+knn_model = None
+kmeans_model = None
+clustered_ratings = None
+
 # Load Data
 def load_data(movies_path, ratings_path):
+    global movies, ratings
     movies = pd.read_csv(movies_path)
     ratings = pd.read_csv(ratings_path)
-    return movies, ratings
 
 # Preprocessing Data
-def preprocess_data(ratings, min_user_votes = 15, min_movie_votes = 31):
+def preprocess_data(ratings, min_user_votes=15, min_movie_votes=31):
     user_counts = ratings['userId'].value_counts()
     movie_counts = ratings['movieId'].value_counts()
     
@@ -102,31 +240,11 @@ def train_svm_model(movies, tfidf_vectorizer):
     svm_model.fit(tfidf_matrix, movies['title'])
     return svm_model, tfidf_matrix
 
-# Movie Recommendation using Linear Kernel SVM
-def get_movie_recommendation_svm(movie_name, model, vectorizer, movies_df, tfidf_matrix, k = 10):
-    movie_idx = movies_df[movies_df['title'].str.contains(movie_name, case=False)].index
-    if len(movie_idx) > 0:
-        movie_features = vectorizer.transform(movies_df.iloc[movie_idx]['genres'])
-        similarity_scores = cosine_similarity(movie_features, tfidf_matrix)
-        similar_movies_idx = np.argsort(similarity_scores[0])[::-1][1:k+1] 
-        recommendations = movies_df.iloc[similar_movies_idx][['title', 'genres']]
-        return recommendations.to_dict('records')
-    else:
-        return [{"Title": "No movies found. Please check your input."}]
-
 # Train KNN Model
 def train_knn_model(user_movie_ratings):
     knn_model = NearestNeighbors(metric='cosine', algorithm='brute')
     knn_model.fit(user_movie_ratings)
     return knn_model
-
-# Movie Recommendation using KNN
-def get_movie_recommendation_knn(movie_id, model, user_movie_ratings, k = 10):
-    distances, indices = model.kneighbors(user_movie_ratings.loc[movie_id].values.reshape(1, -1), n_neighbors = k+1)
-    movie_recommendations = []
-    for i in range(1, len(distances.flatten())):
-        movie_recommendations.append({'Title': user_movie_ratings.index[indices.flatten()[i]], 'Distance': distances.flatten()[i]})
-    return movie_recommendations
 
 # Train K-Means Model
 def train_kmeans_model(user_movie_ratings, num_clusters):
@@ -147,6 +265,26 @@ def recommend_movies(target_user_id, user_movie_ratings, cluster_labels):
     recommendations = top_movies.head(10)
     return recommendations.to_dict()
 
+# Movie Recommendation using Linear Kernel SVM
+def get_movie_recommendation_svm(movie_name, model, vectorizer, movies_df, tfidf_matrix, k = 10):
+    movie_idx = movies_df[movies_df['title'].str.contains(movie_name, case=False)].index
+    if len(movie_idx) > 0:
+        movie_features = vectorizer.transform(movies_df.iloc[movie_idx]['genres'])
+        similarity_scores = cosine_similarity(movie_features, tfidf_matrix)
+        similar_movies_idx = np.argsort(similarity_scores[0])[::-1][1:k+1] 
+        recommendations = movies_df.iloc[similar_movies_idx][['title', 'genres']]
+        return recommendations.to_dict('records')
+    else:
+        return [{"Title": "No movies found. Please check your input."}]
+
+# Movie Recommendation using KNN
+def get_movie_recommendation_knn(movie_id, model, user_movie_ratings, k = 10):
+    distances, indices = model.kneighbors(user_movie_ratings.loc[movie_id].values.reshape(1, -1), n_neighbors = k+1)
+    movie_recommendations = []
+    for i in range(1, len(distances.flatten())):
+        movie_recommendations.append({'Title': user_movie_ratings.index[indices.flatten()[i]], 'Distance': distances.flatten()[i]})
+    return movie_recommendations
+
 # Main Route
 @app.route('/recommend', methods=['GET'])
 def recommend():
@@ -156,7 +294,9 @@ def recommend():
     if movie_name is None or model is None:
         return jsonify({"error": "Please provide both movie and model parameters."}), 400
 
-    if model == 'svm':
+    global movies, ratings, final_dataset, tfidf_vectorizer, svm_model, tfidf_matrix, knn_model, kmeans_model, clustered_ratings
+
+    if model == 'svm_linear':
         recommendations = get_movie_recommendation_svm(movie_name, svm_model, tfidf_vectorizer, movies, tfidf_matrix)
     elif model == 'knn':
         movie_id = int(movie_name)
@@ -165,7 +305,7 @@ def recommend():
         target_user_id = int(movie_name)
         recommendations = recommend_movies(target_user_id, clustered_ratings, kmeans_model.labels_)
     else:
-        return jsonify({"error": "Invalid model parameter. Please choose from 'svm', 'knn', or 'kmeans'."}), 400
+        return jsonify({"error": model}), 400
 
     return jsonify(recommendations)
 
@@ -173,7 +313,7 @@ if __name__ == '__main__':
     # Load Data
     movies_path = 'https://raw.githubusercontent.com/Rakshitx1/Movie-Recomendation-System/master/Dataset/movies.csv'
     ratings_path = 'https://raw.githubusercontent.com/Rakshitx1/Movie-Recomendation-System/master/Dataset/ratings.csv'
-    movies, ratings = load_data(movies_path, ratings_path)
+    load_data(movies_path, ratings_path)
 
     # Preprocess Data
     final_dataset = preprocess_data(ratings)
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,161 +1,201 @@
-// import React from "react";
-// import {
-//   TextField,
-//   Select,
-//   FormControl,
-//   MenuItem,
-//   InputLabel,
-// } from "@mui/material";
+import React from "react";
+import {
+  TextField,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
 
-// import { Button } from "antd";
-// export default function () {
-//   const choices = [
-//     "Content Based Collabrative Filtering",
-//     "Collabrative Filtering",
-//   ];
-//   const [choice, setChoice] = React.useState("");
-//   const [subChoice, setSubChoice] = React.useState("");
-//   const subChoices1 = ["SVM", "Naive Bayes"];
-//   const moreSubChoices1 = ["Linear", "Polynomial", "RBF"];
-//   const subChoices2 = ["KNN", "KMeans", "LR"];
-//   const [movie, setMovie] = React.useState("");
-//   function handleChange(event) {
-//     console.log(event.target.value);
-//     setChoice(event.target.value);
-//   }
-//   function handleSubChange(event) {
-//     console.log(event.target.value);
-//     setSubChoice(event.target.value);
-//   }
-//   function SubChoice1() {
-//     return (
-//       <div className="flex--width">
-//         <FormControl fullWidth className="form">
-//           <InputLabel id="demo-simple-select-label">Choose</InputLabel>
-//           <Select
-//             labelId="demo-simple-select-label"
-//             id="demo-simple-select"
-//             value={subChoice}
-//             label="Choose"
-//             onChange={handleSubChange}
-//           >
-//             <MenuItem value={choices[0]}>
-//               Content Based Collabrative Filtering
-//             </MenuItem>
-//             <MenuItem value={choices[1]}>Collabrative Filtering</MenuItem>
-//           </Select>
-//         </FormControl>
-//       </div>
-//     );
-//   }
-//   function SubChoice2() {
-//     return (
-//       <div>
-//         <FormControl fullWidth className="form">
-//           <InputLabel>Choose</InputLabel>
-//           <Select
-//             //   labelId="demo-simple-select-label"
-//             //   id="demo-simple-select"
-//             value={subChoice}
-//             label="Choose"
-//             onChange={handleSubChange}
-//           >
-//             <MenuItem value={choices[0]}>
-//               Content Based Collabrative Filtering
-//             </MenuItem>
-//             <MenuItem value={choices[1]}>Collabrative Filtering</MenuItem>
-//           </Select>
-//         </FormControl>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div className="flex--column flex--gap">
-//       <TextField
-//         label="Enter the Movie!!!"
-//         onChange={(event) => {
-//           setMovie(event.target.value);
-//         }}
-//         className="text--field"
-//       />
-//       <Button
-//         label="Submit"
-//         type="primary"
-//         onClick={() => {
-//           console.log(movie);
-//         }}
-//       >
-//         Submit
-//       </Button>
-
-//       <h1>Screen</h1>
-//       <FormControl fullWidth className="form">
-//         <InputLabel>Choose</InputLabel>
-//         <Select
-//           //   labelId="demo-simple-select-label"
-//           //   id="demo-simple-select"
-//           value={choice}
-//           label="Choose"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value={choices[0]}>
-//             Content Based Collabrative Filtering
-//           </MenuItem>
-//           <MenuItem value={choices[1]}>Collabrative Filtering</MenuItem>
-//         </Select>
-//       </FormControl>
-//       {choice === "Content Based Collabrative Filtering" && <SubChoice1 />}
-//       {choice === "Collabrative Filtering" && <SubChoice2 />}
-//     </div>
-//   );
-// }
-import React, { useState } from 'react';
-
-export default function() {
-  const [movieName, setMovieName] = useState('');
-  const [recommendations, setRecommendations] = useState([]);
-  const [error, setError] = useState('');
-
-  const fetchRecommendations = () => {
-    fetch(`http://localhost:5000/recommend?movie=${encodeURIComponent(movieName)}`)
-      .then(response => response.json())
-      .then(data => {
+import { Button } from "antd";
+export default function () {
+  const choices = [
+    "Content Based Filtering",
+    "Collabrative Filtering",
+  ];
+  const [currModel, setCurrModel] = React.useState("");
+  const [movieName, setMovieName] = React.useState("");
+  const [recommendations, setRecommendations] = React.useState([]);
+  const [error, setError] = React.useState("");
+  const [choice, setChoice] = React.useState("");
+  const [subChoice, setSubChoice] = React.useState("");
+  const subChoices1 = ["svm", "Naive Bayes"];
+  const [subsubchoice, setSubsubChoice] = React.useState("");
+  const moreSubChoices1 = ["Linear", "Polynomial", "RBF"];
+  const subChoices2 = ["knn", "kmeans", "LR"];
+  const fetchRecommendations = (model) => {
+    fetch(
+      `http://localhost:5000/recommend?movie=${encodeURIComponent(
+        movieName
+      )}&model=${model}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         if (data.error) {
           setError(data.error);
           setRecommendations([]);
         } else {
           setRecommendations(data);
-          setError('');
+          setError("");
         }
       })
-      .catch(error => {
-        console.error('Error:', error);
-        setError('Failed to fetch data. Please try again later.');
+      .catch((error) => {
+        console.error("Error:", error);
+        setError("Failed to fetch data. Please try again later.");
         setRecommendations([]);
       });
   };
+
+  //   const fetchRecommendations = () => {
+  //     fetch(
+  //       `http://localhost:5000/recommend?movie=${encodeURIComponent(movieName)}`
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         if (data.error) {
+  //           setError(data.error);
+  //           setRecommendations([]);
+  //         } else {
+  //           setRecommendations(data);
+  //           setError("");
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error:", error);
+  //         setError("Failed to fetch data. Please try again later.");
+  //         setRecommendations([]);
+  //       });
+  //   };
 
   const handleInputChange = (event) => {
     setMovieName(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();  // Prevent the form from reloading the page
-    fetchRecommendations();
+    event.preventDefault(); // Prevent the form from reloading the page
+    fetchRecommendations(currModel);
   };
-
+  function handleChange(event) {
+    console.log(event.target.value);
+    setChoice(event.target.value);
+  }
+  function handleSubChange(event) {
+    console.log(event.target.value);
+    setSubChoice(event.target.value);
+  }
+  function handleSubSubChange(event) {
+    console.log(event.target.value);
+    setSubsubChoice(event.target.value);
+  }
+  function handleApply() {
+    setCurrModel(subChoice);
+    if (subChoice === "svm" && subsubchoice === "Linear") {
+      setCurrModel("svm_linear");
+    } else if (subChoice === "SVM" && subsubchoice === "Polynomial") {
+      setCurrModel("SVM_Polynomial");
+    } else if (subChoice === "SVM" && subsubchoice === "RBF") {
+      setCurrModel("SVM_RBF");
+    }
+    console.log(currModel);
+  }
+  function SubChoice1() {
+    return (
+      <div className="flex--width">
+        <FormControl className="form select">
+          <InputLabel id="demo-simple-select-label">Choose</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={subChoice}
+            label="Choose"
+            onChange={handleSubChange}
+          >
+            <MenuItem value={subChoices1[0]}>SVM</MenuItem>
+            <MenuItem value={subChoices1[1]}>Naive Bayes</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
+  function SubChoice2() {
+    // const [moreSubChoice, setMoreSubChoice] = React.useState(["KNN"]);
+    return (
+      <div>
+        <FormControl fullWidth className="form select">
+          <InputLabel>Choose</InputLabel>
+          <Select
+            //   labelId="demo-simple-select-label"
+            //   id="demo-simple-select"
+            value={subChoice}
+            label="Choose"
+            onChange={handleSubChange}
+          >
+            <MenuItem value={subChoices2[0]}>KNN</MenuItem>
+            <MenuItem value={subChoices2[1]}>KMeans</MenuItem>
+            <MenuItem value={subChoices2[2]}>LR</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
+  function SubChoice3() {
+    // const [moreSubChoice, setMoreSubChoice] = React.useState(["KNN"]);
+    return (
+      <div>
+        <FormControl fullWidth className="form select">
+          <InputLabel>Choose</InputLabel>
+          <Select
+            //   labelId="demo-simple-select-label"
+            //   id="demo-simple-select"
+            value={subsubchoice}
+            label="Choose"
+            onChange={handleSubSubChange}
+          >
+            <MenuItem value={moreSubChoices1[0]}>Linear</MenuItem>
+            <MenuItem value={moreSubChoices1[1]}>Polynomial</MenuItem>
+            <MenuItem value={moreSubChoices1[2]}>RBF</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Movie Recommendation</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={movieName}
-          onChange={handleInputChange}
-          placeholder="Enter a movie name"
-        />
-        <button type="submit">Get Recommendations</button>
-      </form>
+    <div className="flex--column flex--gap">
+      <TextField
+        label="Enter the Movie!!!"
+        onChange={(event) => {
+          setMovieName(event.target.value);
+        }}
+        className="text--field"
+      />
+      <Button label="Submit" type="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
+
+      <h1>Models</h1>
+      <FormControl className="form select">
+        <InputLabel>Choose</InputLabel>
+        <Select
+          //   id="demo-simple-select"
+          value={choice}
+          label="Choose"
+          onChange={handleChange}
+        >
+          <MenuItem value={choices[0]}>
+            Content Based Filtering
+          </MenuItem>
+          <MenuItem value={choices[1]}>Collabrative Filtering</MenuItem>
+        </Select>
+      </FormControl>
+      <div className="flex--width ">
+        {choice === "Content Based Filtering" && <SubChoice1 />}
+        {choice === "Collabrative Filtering" && <SubChoice2 />}
+
+        {subChoice === "svm" && <SubChoice3 />}
+      </div>
+      <Button type="primary" onClick={handleApply}>
+        Apply
+      </Button>
       {error && <p>Error: {error}</p>}
       <ul>
         {recommendations.map((movie, index) => (
@@ -167,5 +207,61 @@ export default function() {
     </div>
   );
 }
+// import React, { useState } from 'react';
 
+// export default function() {
+//   const [movieName, setMovieName] = useState('');
+//   const [recommendations, setRecommendations] = useState([]);
+//   const [error, setError] = useState('');
 
+//   const fetchRecommendations = () => {
+//     fetch(`http://localhost:5000/recommend?movie=${encodeURIComponent(movieName)}`)
+//       .then(response => response.json())
+//       .then(data => {
+//         if (data.error) {
+//           setError(data.error);
+//           setRecommendations([]);
+//         } else {
+//           setRecommendations(data);
+//           setError('');
+//         }
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//         setError('Failed to fetch data. Please try again later.');
+//         setRecommendations([]);
+//       });
+//   };
+
+//   const handleInputChange = (event) => {
+//     setMovieName(event.target.value);
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();  // Prevent the form from reloading the page
+//     fetchRecommendations();
+//   };
+
+//   return (
+//     <div>
+//       <h1>Movie Recommendation</h1>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={movieName}
+//           onChange={handleInputChange}
+//           placeholder="Enter a movie name"
+//         />
+//         <button type="submit">Get Recommendations</button>
+//       </form>
+//       {error && <p>Error: {error}</p>}
+//       <ul>
+//         {recommendations.map((movie, index) => (
+//           <li key={index}>
+//             {movie.title} - {movie.genres}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
